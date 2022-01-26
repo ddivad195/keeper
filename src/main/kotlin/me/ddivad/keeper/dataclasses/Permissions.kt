64b6af7739eeca49774d16364d91d1e4b7f3d805
ddivad195 <ddivad195@gmail.com>
2022-01-26 18:44:49 +0000

    fix: fix permissions error when config is empty

--- src/main/kotlin/me/ddivad/keeper/dataclasses/Permissions.kt
+++ src/main/kotlin/me/ddivad/keeper/dataclasses/Permissions.kt
@@ -7,13 +7,13 @@ import me.jakejmattson.discordkt.dsl.permission
 @Suppress("unused")
 object Permissions : PermissionSet {
     val BOT_OWNER = permission("Bot Owner") { users(discord.getInjectionObjects<Configuration>().botOwner) }
-    val GUILD_OWNER = permission("Guild Owner") { users(guild!!.ownerId) }
+    val GUILD_OWNER = permission("Guild Owner") { guild?.ownerId?.let { users(it) } }
     val STAFF = permission("Staff") {
         discord.getInjectionObjects<Configuration>()[guild!!.id]?.requiredRoleId?.let {
             roles(it)
         }
     }
-    val NONE = permission("None") { roles(guild!!.everyoneRole.id) }
+    val NONE = permission("None") { guild?.everyoneRole?.let { roles(it.id) } }
 
     override val hierarchy: List<Permission> = listOf(NONE, STAFF, GUILD_OWNER, BOT_OWNER)
     override val commandDefault: Permission = STAFF
